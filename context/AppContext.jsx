1937515a7e25503af9c0ed226913e9e4acb03fd3
mahatolalit <lalitmahato20051024@gmail.com>
2025-07-09 18:17:52 +0530

    Fix: Restrict seller dashboard access to sellers only

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -21,7 +21,7 @@ export const AppContextProvider = (props) => {
 
   const [products, setProducts] = useState([]);
   const [userData, setUserData] = useState(false);
-  const [isSeller, setIsSeller] = useState(true);
+  const [isSeller, setIsSeller] = useState(false);
   const [cartItems, setCartItems] = useState({});
 
   const fetchProductData = async () => {
@@ -38,7 +38,6 @@ export const AppContextProvider = (props) => {
   };
 
   const fetchUserData = async () => {
-    // setUserData(userDummyData);
     try {
       if (user.publicMetadata.role === "seller") {
         setIsSeller(true);
