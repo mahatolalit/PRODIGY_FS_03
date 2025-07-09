6f3936cb49f0c66d86a4cae3ba6a9fd8fa820a6d
mahatolalit <lalitmahato20051024@gmail.com>
2025-07-09 19:12:28 +0530

    Refactor: Update background color of active slide indicator in HeaderSlider

--- components/HeaderSlider.jsx
+++ components/HeaderSlider.jsx
@@ -88,7 +88,7 @@ const HeaderSlider = () => {
             key={index}
             onClick={() => handleSlideChange(index)}
             className={`h-2 w-2 rounded-full cursor-pointer ${
-              currentSlide === index ? "bg-orange-600" : "bg-gray-500/30"
+              currentSlide === index ? "bg-gradient-to-r from-purple-600 to-pink-500" : "bg-gray-500/30"
             }`}
           ></div>
         ))}
